51370b68640b059faedad4437a59e81c43ade7e7
Harish Chauhan <harish.chauhan99@gmail.com>
2018-01-17 14:23:12 +0530

    Create rw_file.py

---
+++ rw_file.py
@@ -0,0 +1,30 @@
+import os
+print(os.getcwd())
+
+curr_dir = os.getcwd()
+
+def write_file():
+    try:
+        my_file = open('sample_file.txt', 'wU')
+        my_file.write('hi hi There There how are you\n')
+        my_file.write('i am Good How Are you')
+        my_file.close()
+        print('data written successfully')
+    except:
+        ('Can not create a file in this directory')
+
+
+
+
+
+def read_file():
+    try:
+        my_file = open('sample_file.txt', 'rU')
+        for line in my_file:
+            print(line)
+
+    except:
+        ('Can not create a file in this directory')
+
+write_file()
+read_file()
