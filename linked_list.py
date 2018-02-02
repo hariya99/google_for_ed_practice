a42d5241212e8250135625a7beacf8654d11ce49
Harish Chauhan <harish.chauhan99@gmail.com>
2018-02-02 16:21:19 +0530

    Create linked_list.py

---
+++ linked_list.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self,data):
+        self.__data=data
+        self.__next=None
+    
+    def get_data(self):
+        return self.__data
+    
+    def set_data(self,data):
+        self.__data=data
+    
+    def get_next(self):
+        return self.__next
+    
+    def set_next(self,next_node):
+        self.__next=next_node
+
+class LinkedList:
+    def __init__(self):
+        self.__head=None
+        self.__tail=None
+    
+    def get_head(self):
+        return self.__head
+    
+    def get_tail(self):
+        return self.__tail
+    
+    def add(self,data):
+        self.new_node = Node(data)
+        self.new_node.set_next(self.__head)
+        self.__head = self.new_node
+        
+    def display(self):
+        """head is at the last object added. get_next gives the address of next added object because this is how it was saved."""
+        current = self.__head
+        while current != None:
+            print(current.get_data())
+            current = current.get_next()
+
+if __name__ == "__main__":
+    list1=LinkedList()
+    list1.add("Sugar")
+    list1.add("TeaBag")
+    list1.add("Salt")
+    print("Element added successfully")
+    list1.display()
